f45ea10b6a6fc2096b76845516bbf6922a1c31d6
ANDREI UDEANU <udeanugheorgheandrei@gmail.com>
2023-04-12 01:13:32 +0200

    Second Version

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
 ];
 
 function time() {
+
     let d = new Date();
     let s = d.getSeconds();
     let m = d.getMinutes();
@@ -16,19 +17,25 @@ function time() {
     timeEl.textContent = 
     day + " " + month + " " + year + "  " + ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
   }
-setInterval(time, 1000);
+setInterval(time, 100);
+
+
+// EXPAND PROJECTS
 
 let state = false
 const projects = document.getElementsByClassName('project');
 const arrow = document.getElementsByClassName('arrow')
 
 const expand = () => {
+
   if (state === false) {
+
     projects[0].style.display = 'block';
     arrow[0].style.transform = 'rotate(0.5turn)';
     state = true
   }
   else {
+
     projects[0].style.display = 'none'
     arrow[0].style.transform = 'rotate(0.0turn)';
     state = false
@@ -36,14 +43,56 @@ const expand = () => {
 }
 
 const expand2 = () => {
+
   if (state === false) {
+
     projects[1].style.display = 'block'
     arrow[1].style.transform = 'rotate(0.5turn)';
     state = true
   }
   else {
+
     projects[1].style.display = 'none'
     arrow[1].style.transform = 'rotate(0.0turn)';
     state = false
   }
+}
+
+
+// HAMBURGER
+
+const hamburger = document.getElementsByClassName('hamburger')[0];
+const nav = document.getElementsByTagName('nav')[0];
+const main = document.getElementsByTagName('main')[0];
+const aList = document.getElementsByTagName('a');
+
+hamburger.onclick = () => {
+
+  if(!nav.active) {
+
+    nav.classList.toggle('active');
+    hamburger.classList.toggle('active');
+  }
+
+  main.onclick = () => {
+
+    if(document.getElementsByClassName('active')) {
+      
+      nav.classList.remove('active');
+      hamburger.classList.remove('active');
+    }
+  }
+
+  for(let a in aList) {
+
+    aList[a].onclick = () => {
+
+      if(document.getElementsByClassName('active')) {
+
+        nav.classList.remove('active');
+        hamburger.classList.remove('active');
+      }
+    }
+  }
+
 }
